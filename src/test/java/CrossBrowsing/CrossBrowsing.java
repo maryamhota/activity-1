a99faa60e0e5a810fbd3d7bed2c8f1e7bc4d27b4
maryamhotak <mhotak.us@gmail.com>
2024-07-10 18:56:40 -0500

    Cross browsing handling

---
+++ src/test/java/CrossBrowsing/CrossBrowsing.java
@@ -0,0 +1,17 @@
+package CrossBrowsing;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CrossBrowsing {
+    public static void main(String[] args) {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https:google.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+
+    }
+}
